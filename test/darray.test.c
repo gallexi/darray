fedda6d419633a9df2092aadc288025f6ab6059c
VictorSCushman <scottcushman@me.com>
2017-10-10 18:07:44 -0400

    Updated unit tests.

--- test/darray.test.c
+++ test/darray.test.c
@@ -7,10 +7,53 @@
 #define INITIAL_NUM_ELEMS 5
 #define RESIZE_NUM_ELEMS 100
 
+EMU_TEST(da_length)
+{
+    struct _darray dastruct;
+
+    dastruct = (struct _darray){.length=7};
+    EMU_EXPECT_EQ_UINT(da_length(dastruct.data), 7);
+
+    dastruct = (struct _darray){.length=42};
+    EMU_EXPECT_EQ_UINT(da_length(dastruct.data), 42);
+
+    EMU_END_TEST();
+}
+
+EMU_TEST(da_capacity)
+{
+    struct _darray dastruct;
+
+    dastruct = (struct _darray){.capacity=7};
+    EMU_EXPECT_EQ_UINT(da_capacity(dastruct.data), 7);
+
+    dastruct = (struct _darray){.capacity=42};
+    EMU_EXPECT_EQ_UINT(da_capacity(dastruct.data), 42);
+
+    EMU_END_TEST();
+}
+
+EMU_TEST(da_sizeof_elem)
+{
+    struct _darray dastruct;
+
+    dastruct = (struct _darray){.elemsz=4};
+    EMU_EXPECT_EQ_UINT(da_sizeof_elem(dastruct.data), 4);
+
+    dastruct = (struct _darray){.elemsz=32};
+    EMU_EXPECT_EQ_UINT(da_sizeof_elem(dastruct.data), 32);
+
+    EMU_END_TEST();
+}
+
 EMU_TEST(da_alloc__and__da_free)
 {
     int* da = da_alloc(INITIAL_NUM_ELEMS, sizeof(int));
     EMU_REQUIRE_NOT_NULL(da);
+    EMU_REQUIRE_EQ_UINT(da_length(da), INITIAL_NUM_ELEMS);
+    EMU_REQUIRE_GE_UINT(da_capacity(da), INITIAL_NUM_ELEMS);
+    EMU_REQUIRE_EQ_UINT(da_sizeof_elem(da), sizeof(int));
+
     for (size_t i = 0; i < INITIAL_NUM_ELEMS; ++i) // writiable without crashing
     {
         da[i] = i;
@@ -24,12 +67,14 @@ EMU_TEST(da_alloc_exact__and__da_free)
 {
     int* da = da_alloc_exact(INITIAL_NUM_ELEMS, sizeof(int));
     EMU_REQUIRE_NOT_NULL(da);
+    EMU_REQUIRE_EQ_UINT(da_length(da), INITIAL_NUM_ELEMS);
+    EMU_REQUIRE_EQ_UINT(da_capacity(da), INITIAL_NUM_ELEMS);
+    EMU_REQUIRE_EQ_UINT(da_sizeof_elem(da), sizeof(int));
+
     for (size_t i = 0; i < INITIAL_NUM_ELEMS; ++i) // writiable without crashing
     {
         da[i] = i;
     }
-    EMU_EXPECT_EQ_UINT(da_length(da), INITIAL_NUM_ELEMS);
-    EMU_EXPECT_EQ_UINT(da_capacity(da), INITIAL_NUM_ELEMS);
 
     da_free(da);
     EMU_END_TEST();
@@ -40,47 +85,6 @@ EMU_GROUP(alloc_and_free_functions)
     EMU_ADD(da_alloc__and__da_free);
     EMU_ADD(da_alloc_exact__and__da_free);
     EMU_END_GROUP();
-}
-
-EMU_TEST(da_length)
-{
-    int* da = da_alloc(INITIAL_NUM_ELEMS, sizeof(int));
-
-    EMU_EXPECT_EQ_UINT(da_length(da), INITIAL_NUM_ELEMS);
-    EMU_PRINT_INDENT();
-    printf("length  : %zu\n", da_length(da));
-
-    da_free(da);
-    EMU_END_TEST();
-}
-
-EMU_TEST(da_capacity)
-{
-    int* da = da_alloc(INITIAL_NUM_ELEMS, sizeof(int));
-
-    EMU_EXPECT_GE_UINT(da_capacity(da), INITIAL_NUM_ELEMS);
-    EMU_PRINT_INDENT();
-    printf("capacity : %zu\n", da_capacity(da));
-
-    da_free(da);
-    EMU_END_TEST();
-}
-
-EMU_TEST(da_sizeof_elem)
-{
-    int*   da1 = da_alloc(INITIAL_NUM_ELEMS, sizeof(int));
-    void*  da2 = da_alloc(INITIAL_NUM_ELEMS, sizeof(int));
-    float* da3 = da_alloc(INITIAL_NUM_ELEMS, sizeof(float));
-
-    EMU_EXPECT_EQ_UINT(da_sizeof_elem(da1), sizeof(int));
-    EMU_EXPECT_EQ_UINT(da_sizeof_elem(da2), sizeof(int));
-    EMU_EXPECT_EQ_UINT(da_sizeof_elem(da3), sizeof(float));
-
-    da_free(da1);
-    da_free(da2);
-    da_free(da3);
-
-    EMU_END_TEST();
 }
 
 EMU_TEST(da_resize)
@@ -114,8 +118,8 @@ EMU_TEST(da_resize_exact)
 
     da = da_resize_exact(da, RESIZE_NUM_ELEMS);
     EMU_REQUIRE_NOT_NULL(da);
-    EMU_EXPECT_EQ_UINT(da_length(da), RESIZE_NUM_ELEMS);
-    EMU_EXPECT_EQ_UINT(da_capacity(da), RESIZE_NUM_ELEMS);
+    EMU_REQUIRE_EQ_UINT(da_length(da), RESIZE_NUM_ELEMS);
+    EMU_REQUIRE_EQ_UINT(da_capacity(da), RESIZE_NUM_ELEMS);
     for (size_t i = 0; i < INITIAL_NUM_ELEMS; ++i)
     {
         EMU_EXPECT_EQ_INT(da[i], (int)i);
@@ -127,14 +131,14 @@ EMU_TEST(da_resize_exact)
 
 EMU_TEST(da_reserve)
 {
-    int* da = da_alloc(INITIAL_NUM_ELEMS, sizeof(int));
+    int* da = da_alloc(1, sizeof(int));
 
-    da = da_reserve(da, 50);
+    da = da_reserve(da, 5000);
     EMU_REQUIRE_NOT_NULL(da);
-    EMU_EXPECT_GE_UINT(da_capacity(da)-da_length(da), 50);
+    EMU_REQUIRE_GE_UINT(da_capacity(da)-da_length(da), 5000);
 
-    // multiple calls to reserve with the same reserve nelem should only cause
-    // the array to change the first time.
+    // Multiple calls to reserve with the same reserve nelem should only cause
+    // the darray to change the first time.
     int* da2 = da_reserve(da, 50);
     EMU_REQUIRE_NOT_NULL(da);
     EMU_EXPECT_EQ(da2, da);
@@ -153,8 +157,8 @@ EMU_TEST(da_push)
         EMU_EXPECT_TRUE(da_push(da, i));
     }
     EMU_REQUIRE_NOT_NULL(da);
-    EMU_EXPECT_EQ_UINT(da_length(da), max_index+1);
-    EMU_EXPECT_GE_UINT(da_capacity(da),da_length(da));
+    EMU_REQUIRE_EQ_UINT(da_length(da), max_index+1);
+    EMU_REQUIRE_GE_UINT(da_capacity(da),da_length(da));
     for (int i = 0; i <= max_index; ++i)
     {
         EMU_EXPECT_EQ_INT(da[i], i);
@@ -172,11 +176,11 @@ EMU_TEST(da_pop)
 
     EMU_EXPECT_EQ_INT(da_pop(da), 5);
     EMU_REQUIRE_NOT_NULL(da);
-    EMU_EXPECT_EQ_UINT(da_length(da), 1);
+    EMU_REQUIRE_EQ_UINT(da_length(da), 1);
 
     EMU_EXPECT_EQ_INT(da_pop(da), 3);
     EMU_REQUIRE_NOT_NULL(da);
-    EMU_EXPECT_EQ_UINT(da_length(da), 0);
+    EMU_REQUIRE_EQ_UINT(da_length(da), 0);
 
     da_free(da);
     EMU_END_TEST();
@@ -188,16 +192,16 @@ EMU_TEST(da_insert__basic)
     da[0] = 3;
     da[1] = 5;
 
-    EMU_EXPECT_TRUE(da_insert(da, 0, 7));
+    EMU_REQUIRE_TRUE(da_insert(da, 0, 7));
     EMU_REQUIRE_NOT_NULL(da);
-    EMU_EXPECT_EQ_UINT(da_length(da), 3);
+    EMU_REQUIRE_EQ_UINT(da_length(da), 3);
     EMU_EXPECT_EQ_INT(da[0], 7);
     EMU_EXPECT_EQ_INT(da[1], 3);
     EMU_EXPECT_EQ_INT(da[2], 5);
 
-    EMU_EXPECT_TRUE(da_insert(da, 1, 9));
+    EMU_REQUIRE_TRUE(da_insert(da, 1, 9));
     EMU_REQUIRE_NOT_NULL(da);
-    EMU_EXPECT_EQ_UINT(da_length(da), 4);
+    EMU_REQUIRE_EQ_UINT(da_length(da), 4);
     EMU_EXPECT_EQ_INT(da[0], 7);
     EMU_EXPECT_EQ_INT(da[1], 9);
     EMU_EXPECT_EQ_INT(da[2], 3);
@@ -214,10 +218,10 @@ EMU_TEST(da_insert__mimic_push_front)
 
     for (int i = max_index; i >= 0; --i)
     {
-        EMU_EXPECT_TRUE(da_insert(da, 0, i));
+        EMU_REQUIRE_TRUE(da_insert(da, 0, i));
         EMU_REQUIRE_NOT_NULL(da);
     }
-    EMU_EXPECT_EQ_UINT(da_length(da), max_index+1);
+    EMU_REQUIRE_EQ_UINT(da_length(da), max_index+1);
     for (int i = max_index; i >= 0; --i)
     {
         EMU_EXPECT_EQ_INT(da[i], i);
@@ -234,38 +238,36 @@ EMU_GROUP(da_insert)
     EMU_END_GROUP();
 }
 
-struct bigstruct {int A[500];};
-
 EMU_TEST(da_remove)
 {
-    int* da = da_alloc(4, sizeof(int));
-    da[0] = 3;
-    da[1] = 5;
-    da[2] = 7;
-    da[3] = 9;
+    int* da1 = da_alloc(4, sizeof(int));
+    da1[0] = 3;
+    da1[1] = 5;
+    da1[2] = 7;
+    da1[3] = 9;
 
     // remove from middle
-    EMU_EXPECT_EQ_INT(da_remove(da, 1), 5);
-    EMU_REQUIRE_NOT_NULL(da);
-    EMU_EXPECT_EQ_UINT(da_length(da), 3);
+    EMU_EXPECT_EQ_INT(da_remove(da1, 1), 5);
+    EMU_REQUIRE_NOT_NULL(da1);
+    EMU_EXPECT_EQ_UINT(da_length(da1), 3);
 
     // remove from front
-    EMU_EXPECT_EQ_INT(da_remove(da, 0), 3);
-    EMU_REQUIRE_NOT_NULL(da);
-    EMU_EXPECT_EQ_UINT(da_length(da), 2);
+    EMU_EXPECT_EQ_INT(da_remove(da1, 0), 3);
+    EMU_REQUIRE_NOT_NULL(da1);
+    EMU_EXPECT_EQ_UINT(da_length(da1), 2);
 
     // remove from back
-    EMU_EXPECT_EQ_INT(da_remove(da, 1), 9);
-    EMU_REQUIRE_NOT_NULL(da);
-    EMU_EXPECT_EQ_UINT(da_length(da), 1);
+    EMU_EXPECT_EQ_INT(da_remove(da1, 1), 9);
+    EMU_REQUIRE_NOT_NULL(da1);
+    EMU_EXPECT_EQ_UINT(da_length(da1), 1);
 
-    struct bigstruct* bda = da_alloc(4, sizeof(struct bigstruct));
-    da_remove(bda, 1); // remove from middle
-    da_remove(bda, 0); // remove from front
-    da_remove(bda, 1); // remove from back
+    int* da2 = da_alloc(4, sizeof(int));
+    da_remove(da2, 1); // remove from middle
+    da_remove(da2, 0); // remove from front
+    da_remove(da2, 1); // remove from back
 
-    da_free(da);
-    da_free(bda);
+    da_free(da1);
+    da_free(da2);
     EMU_END_TEST();
 }
 
@@ -277,17 +279,17 @@ EMU_TEST(da_swap)
     da[5] = 99;
 
     da_swap(da, 3, 5);
-    EMU_EXPECT_EQ_INT(da[3], 99);
-    EMU_EXPECT_EQ_INT(da[5], 12);
+    EMU_REQUIRE_EQ_INT(da[3], 99);
+    EMU_REQUIRE_EQ_INT(da[5], 12);
 
     da_swap(da, 3, 5);
-    EMU_EXPECT_EQ_INT(da[3], 12);
-    EMU_EXPECT_EQ_INT(da[5], 99);
+    EMU_REQUIRE_EQ_INT(da[3], 12);
+    EMU_REQUIRE_EQ_INT(da[5], 99);
 
     // swap element with itself
     da_swap(da, 3, 3);
-    EMU_EXPECT_EQ_INT(da[3], 12);
-    EMU_EXPECT_EQ_INT(da[5], 99);
+    EMU_REQUIRE_EQ_INT(da[3], 12);
+    EMU_REQUIRE_EQ_INT(da[5], 99);
 
     da_free(da);
     EMU_END_TEST();
@@ -307,7 +309,7 @@ EMU_TEST(da_cat__darray_cat)
     dest = da_cat(dest, src, 2);
     EMU_REQUIRE_NOT_NULL(dest);
     EMU_REQUIRE_NOT_NULL(src);
-    EMU_EXPECT_EQ_UINT(da_length(dest), 5);
+    EMU_REQUIRE_EQ_UINT(da_length(dest), 5);
     for (size_t i = 0; i < 5; ++i)
     {
         EMU_EXPECT_EQ_INT(dest[i], i);
@@ -330,7 +332,7 @@ EMU_TEST(da_cat__array_cat)
     dest = da_cat(dest, src, 2);
     EMU_REQUIRE_NOT_NULL(dest);
     EMU_REQUIRE_NOT_NULL(src);
-    EMU_EXPECT_EQ_UINT(da_length(dest), 5);
+    EMU_REQUIRE_EQ_UINT(da_length(dest), 5);
     for (size_t i = 0; i < 5; ++i)
     {
         EMU_EXPECT_EQ_INT(dest[i], i);
@@ -352,10 +354,7 @@ EMU_TEST(da_cat__cstring_cat)
     EMU_EXPECT_STREQ(dest, "Hello World!");
     EMU_EXPECT_EQ_UINT(da_length(dest), strlen("Hello World!")+1);
 
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wunused-value"
     da_pop(dest); // remove null terminator
-    #pragma GCC diagnostic pop
 
     char another[] = " Another one!";
     dest = da_cat(dest, another, strlen(another)+1);
@@ -379,7 +378,7 @@ EMU_TEST(da_fill__const_value)
 {
     int* da = da_alloc(INITIAL_NUM_ELEMS, sizeof(int));
 
-    // fill with different values
+    // prefill with different values
     for (size_t i = 0; i < da_length(da); ++i)
     {
         da[i] = i;
@@ -399,7 +398,7 @@ EMU_TEST(da_fill__rand_value)
 {
     int* da = da_alloc(INITIAL_NUM_ELEMS, sizeof(int));
 
-    // fill with different values
+    // prefill with different values
     for (size_t i = 0; i < da_length(da); ++i)
     {
         da[i] = i;
@@ -444,15 +443,12 @@ EMU_TEST(da_foreach__iterates_forward)
 {
     int* da = da_alloc(INITIAL_NUM_ELEMS, sizeof(int));
 
-    for (size_t i = 0; i < da_length(da); ++i){da[i] = 0;}
+    for (size_t i = 0; i < da_length(da); ++i){da[i] = i;}
+    int last = -1;
     da_foreach(da, iter)
     {
-        if (iter != da)
-            *iter = *(iter-1) + 1;
-    }
-    for (size_t i = 1; i < da_length(da); ++i)
-    {
-        EMU_EXPECT_EQ_INT(da[i], da[i-1] + 1);
+        EMU_EXPECT_GE_INT(*iter, last);
+        last = *iter;
     }
 
     da_free(da);
@@ -520,10 +516,10 @@ EMU_TEST(container_style_type)
 
 EMU_GROUP(darray_functions)
 {
-    EMU_ADD(alloc_and_free_functions);
     EMU_ADD(da_length);
     EMU_ADD(da_capacity);
     EMU_ADD(da_sizeof_elem);
+    EMU_ADD(alloc_and_free_functions);
     EMU_ADD(da_resize);
     EMU_ADD(da_resize_exact);
     EMU_ADD(da_reserve);
@@ -546,7 +542,7 @@ struct foo
     double c;
 };
 
-EMU_TEST(small_struct)
+EMU_TEST(struct_type)
 {
     struct foo* da = da_alloc(2, sizeof(struct foo));
     EMU_REQUIRE_NOT_NULL(da);
@@ -578,7 +574,7 @@ EMU_TEST(cstrings)
 
 EMU_GROUP(testing_with_different_types)
 {
-    EMU_ADD(small_struct);
+    EMU_ADD(struct_type);
     EMU_ADD(cstrings);
     EMU_END_GROUP();
 }
